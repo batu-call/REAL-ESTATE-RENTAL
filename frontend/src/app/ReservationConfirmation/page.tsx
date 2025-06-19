697103126ba1fb02fbfddf4d802afdd58eb3c95d
Batuhan Çallıoğlu <batucalll3@gmail.com>
2025-06-19 18:21:44 +0300

    suspense

--- frontend/src/app/ReservationConfirmation/page.tsx
+++ frontend/src/app/ReservationConfirmation/page.tsx
@@ -1,5 +1,17 @@
-import ReservationConfirmationClient from "./ReservationConfirmationClient";
+'use client';
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+
+const ReservationConfirmationClient = dynamic(() => import("./ReservationConfirmationClient"), {
+  ssr: false,
+});
 
 export default function Page() {
-  return <ReservationConfirmationClient />;
+  return (
+    <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
+      <ReservationConfirmationClient />
+    </Suspense>
+  );
 }
